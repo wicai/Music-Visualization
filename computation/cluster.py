61255626d80c3985eaad6d8d9054209ce8053eb4
William Cai <caimaster1@gmail.com>
2015-01-17 06:14:02 -0500

    computing.py is done

--- computation/cluster.py
+++ computation/cluster.py
@@ -2,11 +2,16 @@
 import csv
 import numpy
 import sklearn.preprocessing as preprocessing
+from sklearn.decomposition import PCA as PCA
+from sklearn.cluster import k_means as k_means
 
 songid_file = open("../static/songids.csv", "rU")
 reader = csv.reader(songid_file)
 features = []
+other = []
+
 for row in reader:
+    other_row = [row[1], row[2]]
     acousticness = float(row[3])
     danceability = float(row[4])
     duration = float(row[5])
@@ -18,9 +23,20 @@ for row in reader:
     tempo = float(row[11])
     feature_arr = [acousticness, danceability, duration, energy, liveness, loudness, mode, speechiness, tempo]
     features.append(feature_arr)
-
+    other.append(other_row)
 print features
+print other
 min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
 trained=min_max_scaler.fit_transform(features)
+print trained
 
+pca = PCA(copy=True, n_components=3, whiten=False)
+pca.fit(trained)
+
+pcaed = pca.fit_transform(trained)
+print pcaed
+
+n_clusters = 3
+returned = k_means(pcaed, n_clusters=n_clusters) 
+print returned
 
---
+++ env/lib/python2.7/site-packages/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.15.0'
+version = '0.15.0'
+full_version = '0.15.0'
+git_revision = '88fc9b098c4e9604b280f16f7c5530b81ab1d45c'
+release = True
+
+if not release:
+    version = full_version
