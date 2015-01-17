edd6937e62eba64ae10a923502d593c07f892838
Daniel Chen <chen.daniel7@gmail.com>
2015-01-17 05:00:02 -0500

    saved features in csv

---
+++ SpotifyData.py
@@ -0,0 +1,85 @@
+import sys
+import spotipy
+import spotipy.util as util
+import csv
+import urllib, urllib2
+import json
+from pylab import plot,show
+from numpy import vstack,array
+from numpy.random import rand
+from scipy.cluster.vq import kmeans,vq
+
+# export SPOTIPY_CLIENT_ID='562a7296affa4b5dbe70437d11d837e3'
+# export SPOTIPY_CLIENT_SECRET='0153de287f2c45e0846c0390b67f991d'
+# export SPOTIPY_REDIRECT_URI='http://127.0.0.1:5000/login/authorized'
+
+scope = 'user-library-read'
+username = 'charmip'
+api_key = 'PV1DZKHWQ6OZX6LEO'
+
+
+
+def show_tracks(results):
+    songid_file = open("/Users/danielchen/Documents/Music-Visualization/static/songids.csv", 'w')
+    writer = csv.writer(songid_file)
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print "   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name'])
+        url = "http://developer.echonest.com/api/v4/song/search"
+        data = {}
+        data['api_key'] = api_key
+        artist = ("%32.32s" % (track['artists'][0]['name'])).encode("utf8")
+        data['artist'] = artist
+        name = track['name']
+        data['title'] = name
+        url_values = urllib.urlencode(data)
+        full_url = url + '?' + url_values
+        ids = urllib2.urlopen(full_url).read()
+        json_response = json.loads(ids)
+        song_id = json_response["response"]["songs"][0]["id"]
+
+        url2 = "http://developer.echonest.com/api/v4/song/profile"
+        data2 = {}
+        data2['api_key'] = api_key
+        data2['id'] = song_id
+        data2['bucket'] = 'audio_summary'
+        url_values2 = urllib.urlencode(data2)
+        full_url2 = url2 + '?' + url_values2
+        features = urllib2.urlopen(full_url2).read()
+        json_response2 = json.loads(features)
+
+
+        acousticness = json_response2["response"]["songs"][0]["audio_summary"]["acousticness"]
+        danceability = json_response2["response"]["songs"][0]["audio_summary"]["danceability"]
+        duration = json_response2["response"]["songs"][0]["audio_summary"]["duration"]
+        energy = json_response2["response"]["songs"][0]["audio_summary"]["energy"]
+        liveness = json_response2["response"]["songs"][0]["audio_summary"]["liveness"]
+        loudness = json_response2["response"]["songs"][0]["audio_summary"]["loudness"]
+        mode = json_response2["response"]["songs"][0]["audio_summary"]["mode"]
+        speechiness = json_response2["response"]["songs"][0]["audio_summary"]["speechiness"]
+        tempo = json_response2["response"]["songs"][0]["audio_summary"]["tempo"]
+        feature_arr = [song_id, name, artist, acousticness, danceability, duration, energy, liveness, loudness, mode, speechiness, tempo]
+        
+        print feature_arr
+        writer.writerow(feature_arr)
+
+
+token = util.prompt_for_user_token(username, scope)
+if token:
+    sp = spotipy.Spotify(auth=token)
+    playlists = sp.user_playlists(username)
+    for playlist in playlists['items']:
+            if playlist['owner']['id'] == username:
+                print playlist['name']
+                print '  total tracks', playlist['tracks']['total']
+                results = sp.user_playlist(username, playlist['id'],
+                    fields="tracks,next")
+                tracks = results['tracks']
+                show_tracks(tracks)
+                while tracks['next']:
+                    tracks = sp.next(tracks)
+                    show_tracks(tracks)
+                break
+else:
+    print "Can't get token for", username
--- music_cluster.py
+++ music_cluster.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask_oauthlib.client import OAuth, OAuthException
 from contextlib import closing
 import urllib
 import urllib2
@@ -12,9 +13,16 @@ DEBUG = True
 USERNAME = 'test'
 PASSWORD = 'test'
 
-
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.secret_key = 'why would I tell you my secret key?'
+oauth = OAuth(app)
+SPOTIPY_CLIENT_ID='562a7296affa4b5dbe70437d11d837e3'
+SPOTIPY_CLIENT_SECRET='0153de287f2c45e0846c0390b67f991d'
+
+scope = 'user-library-read'
+username = 'charmip'
+
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
@@ -25,9 +33,61 @@ def init_db():
 			db.cursor().executescript(f.read())
 		db.commit()
 
+spotify = oauth.remote_app(
+    'spotify',
+    consumer_key=SPOTIPY_CLIENT_ID,
+    consumer_secret=SPOTIPY_CLIENT_SECRET,
+    # Change the scope to match whatever it us you need
+    # list of scopes can be found in the url below
+    # https://developer.spotify.com/web-api/using-scopes/
+    request_token_params={username: username, scope: scope},
+    base_url='https://accounts.spotify.com',
+    request_token_url=None,
+    access_token_url='/api/token',
+    authorize_url='https://accounts.spotify.com/authorize'
+)
+
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return redirect(url_for('login'))
+
+
+@app.route('/login')
+def login():
+    callback = url_for(
+        'spotify_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True
+    )
+    print callback
+    return spotify.authorize(callback=callback)
+
+
+@app.route('/login/authorized')
+def spotify_authorized():
+    resp = spotify.authorized_response()
+    if resp is None:
+        return 'Access denied: reason={0} error={1}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    if isinstance(resp, OAuthException):
+        return 'Access denied: {0}'.format(resp.message)
+
+    session['oauth_token'] = (resp['access_token'], '')
+    me = spotify.get('/me')
+    return 'Logged in as id={0} name={1} redirect={2}'.format(
+        me.data['id'],
+        me.data['name'],
+        request.args.get('next')
+    )
+
+
+@spotify.tokengetter
+def get_spotify_oauth_token():
+    return session.get('oauth_token')
+
 
 if __name__ == '__main__':
 	app.run()
