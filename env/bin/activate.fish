1eefffb7da31284cc5badf87f700918ad320e1df
William Cai <caimaster1@gmail.com>
2015-01-17 14:28:10 -0500

    cleaned env

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/williamcai/musicproject/env"
+set -gx VIRTUAL_ENV "/Users/williamcai/musicproject/Music-Visualization/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
