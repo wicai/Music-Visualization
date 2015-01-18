201765b8151b7da559e6e79f8f41b66ff7025116
Charmi Patel <charmip@umd.edu>
2015-01-18 00:07:07 -0500

    Spotify ids for playlist found

--- playlist.py
+++ playlist.py
@@ -8,6 +8,7 @@ echonest_taste_profile_create_api = 'http://developer.echonest.com/api/v4/tastep
 echonest_taste_profile_update_api = 'http://developer.echonest.com/api/v4/tasteprofile/update'
 
 api_key = "4L4M7QV9W0QSNZ2UD"
+spotify_auth_code = "BQDY3Ccu7vobaCfAKh7lUPwNN-W8xtHDV_ajyjenARigS6d64k274dzQbe8noLNl5Sku6_zHQdtN2waRCgybcqdrq2FqUzSt3X95KGZa_7MPaJppeDHKVkFuGwWPAFGCFX2vVoHfplFAIrtudHnwqZF1TgQB_E3ZBM6LwhfCJr8OlTnKdslJKMU"
 
 def find_data():
     url = 'http://developer.echonest.com/api/v4/song/search?api_key=FILDTEOIK2HBORODV&artist=kanye%20west&title=all%20of%20the%20lights'
@@ -90,18 +91,10 @@ def create_taste_profile(songs):
         update_data = urllib.urlencode(update_values)
         update_req = urllib2.Request(echonest_taste_profile_update_api, update_data)
         try:
-            update_response = urllib2.urlopen(req)
-            print(update_response.read())
+            update_response = urllib2.urlopen(update_req)
         except urllib2.URLError as update_e:
             print(update_e.reason)
-        
-        
-        # u = 'http://developer.echonest.com/api/v4/tasteprofile/read?api_key=4L4M7QV9W0QSNZ2UD&format=json&id=' + catalog_id + '&bucket=audio_summary'
-        # print(u)
-        # u1 = urllib2.urlopen(u)
-        # p = u1.read()
-        # print(p)
-        
+
         return catalog_id
         
     except urllib2.URLError as e:
@@ -116,16 +109,24 @@ def create_playlist(taste_profile_id):
     }
     
     data = urllib.urlencode(values)
-    req = urllib2.Request(echonest_playlists_api, data)
+    req = echonest_playlists_api + data + '&bucket=tracks&bucket=id:spotify&limit=true'
+   
     try:
-        print(data)
         response = urllib2.urlopen(req)
+        playlist_data = json.loads(response.read())
+        songs = playlist_data["response"]["songs"]
+       
+        uris = []
+        for s in songs:
+            track_id = s["tracks"][0]["foreign_id"]
+            uris.append(str(track_id))
         
+        uris_json = {"uris": uris}
+        return uris_json
+
     except urllib2.URLError as e:
         print(e.reason)
 
-
 songs = find_songs(0.9101, 0.1515, 401.30667, 0.2253, 0.07785, -15.4083, 1, 0.0354, 83.3769)
 taste_profile_id = create_taste_profile(songs)
-#create_playlist(taste_profile_id)
-# find_data()
+uris_json = create_playlist(taste_profile_id)
